12e8de459319ef36eb3c385c4aa250a2facf126f
kwanele-hub <kwanelemkhwanazi688@gmail.com>
2020-10-07 13:10:01 +0000

    testing

---
+++ testing.js
@@ -0,0 +1,24 @@
+//assign a variable name then assign it to a name
+var name= "Lholho"
+name  = "Clie"
+console.log(name)
+
+
+//split the following: Welcome to this Javascript Guide! and join it
+var string = "Welcome to this Javascript Guide!";
+var reverseString = string.split('').splice('').join('');
+console.log("")
+
+//Multiply all numbers in an array by 4
+var numbers=[2,4,6,8]
+var multiplied=[]
+for (var i = 0; i < numbers.length; i++){
+ multiplied.push(numbers[i] * 4)
+}
+console.log(multiplied)
+
+//Print out numbers from 0-20
+for (var i = 0; i <= 20; i++) {
+ 
+}
+console.log(i);
