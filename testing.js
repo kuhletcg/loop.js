a939b5c83bddd0f084bff52c25e443b2c6bad009
TCG <ssibonokuhle@gmail.com>
2021-04-09 13:14:15 +0200

    Added objects

--- testing.js
+++ testing.js
@@ -25,3 +25,23 @@ for (var i = 0; i <= 20; i++) {
 var string = "Welcome to this Javascript Guide!";
 var reverseString = string.split(" ").reverse().join(" ");
 console.log(reverseString);
+
+// objects
+function hasProp(name, checkName) {
+  //   var res = false;
+  if (name != checkName) {
+    console.log(`value ${name} != ${checkName} `);
+  } else {
+    console.log(`value ${name} == ${checkName} `);
+  }
+}
+
+var ts1 = hasProp(obj.name, "mark");
+var ts2 = hasProp(obj.name, "name");
+
+console.log(`ts1`, ts1);
+console.log(`ts2`, ts2);
+
+// object
+var obj = { name: "mark", middleName: "max", lastName: "Jax" };
+var obj2 = { middleName: "max", lastName: "Jax" };
