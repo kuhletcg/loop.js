bd7f1e19f481e9f863f2ec7397debb80fa18cd3e
TCG <ssibonokuhle@gmail.com>
2021-05-13 14:42:57 +0200

    Adde function that return even numbers

--- testing.js
+++ testing.js
@@ -94,3 +94,23 @@ const findLargestNumbers = (arr) => {
   return twoLargest;
 };
 console.log(findLargestNumbers([14, 26, 10]));
+
+// even numbers
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 40, 20];
+
+const getAllEvenNumbers = (numbers) => {
+  var evenNumbers = [];
+
+  for (var i in numbers) {
+    if (numbers[i] / 2) {
+      evenNumbers.push(numbers[i]);
+      return evenNumbers;
+    }
+  }
+};
+
+const getAllEvenNumbers = (numbers) => {
+  return numbers.filter((number) => number % 2 == 0);
+};
+
+console.log(getAllEvenNumbers(numbers));
