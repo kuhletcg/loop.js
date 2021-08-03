fdf05874a3fc025615d79109768ee8db3984500b
TCG <ssibonokuhle@gmail.com>
2021-08-03 11:15:26 +0200

    Check if an element exists in the array

--- testing.js
+++ testing.js
@@ -114,3 +114,14 @@ const getAllEvenNumbers = (numbers) => {
 };
 
 console.log(getAllEvenNumbers(numbers));
+
+
+//Check if an element exists in the array
+function exists(value, array) {
+    return array.some(e => e === value);
+}
+
+let marks = [4, 5, 7, 9, 10, 2];
+
+console.log(exists(4, marks));
+console.log(exists(11, marks));
